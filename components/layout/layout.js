694731d8a4e6b1c57d9264255a6e6f5f2b5ca888
Mario Vinti <mjvinti@gmail.com>
2023-03-10 11:33:21 -0500

    added layout and navigation

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import MainNavigation from "./main-navigation";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <MainNavigation />
+      <main></main>
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/layout/main-navigation.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+import Logo from "./logo";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
