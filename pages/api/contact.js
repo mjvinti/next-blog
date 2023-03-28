d903818acbc176119dd75caa739888402c652050
Mario Vinti <mjvinti@gmail.com>
2023-03-28 16:01:26 -0400

    added contact api route

---
+++ pages/api/contact.js
@@ -0,0 +1,24 @@
+function handler(req, res) {
+  const {
+    body: { email, message, name },
+    method,
+  } = req;
+
+  if (method === "POST") {
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      return res.status(422).json({ message: "Invalid input." });
+    }
+
+    // store in a database
+    return res.status(201).json({ message: "Successfully stored message!" });
+  }
+}
+
+export default handler;
